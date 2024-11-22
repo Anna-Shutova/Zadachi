7349772884b3dd22e757c3bcc4ba5e0987231d1c
Anna-Shutova <annlovebeauty@gmail.com>
2024-11-22 18:49:43 +0000

    сделала дз10

---
+++ dz10/task3.py
@@ -0,0 +1,11 @@
+count = 0
+
+for number in range(100, 10000):
+    digits = str(number)
+
+    if (digits[0] != digits[1]) != digits[2]) or \
+        (digits[0] != di)
+        count += 1
+        print(digits)
+
+print(count)
