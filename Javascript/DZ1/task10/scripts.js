e33f07e57942e7d8b379c60099aae6319a9334c4
Anna <annlovebeauty@gmail.com>
2025-01-17 21:38:17 +0300

    task10

---
+++ Javascript/DZ1/task10/scripts.js
@@ -0,0 +1,12 @@
+document.getElementById('Button').addEventListener('click', function() {
+    const number = document.getElementById('nameInput').value;
+    const message = document.getElementById('message');
+
+    if (number % 2 == 0) {
+        alert("четное");
+        message.textContent = "Правильно";
+    } else {
+        alert('нечетное')
+        message.textContent = 'Введите четное число!!!!!';
+    }
+});
